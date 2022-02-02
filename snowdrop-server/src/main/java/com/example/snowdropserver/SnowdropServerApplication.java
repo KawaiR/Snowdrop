d13bf8369f6286314609a096d6abe85f5b013688
Ramya Mandyam Anandampullai <42981906+rmandyam1@users.noreply.github.com>
2022-02-01 19:13:55 -0500

    Server set up

---
+++ snowdrop-server/src/main/java/com/example/snowdropserver/SnowdropServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.snowdropserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowdropServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowdropServerApplication.class, args);
+    }
+
+}
---
+++ snowdrop-server/src/test/java/com/example/snowdropserver/SnowdropServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snowdropserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowdropServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
