62a6d973526f71c94c7096eb219f361122f7e334
Jihyun Pyun <pyunj30@gmail.com>
2022-02-13 03:32:54 -0500

    add stuff

--- snowdrop-client/assets/PlantsPage/PlantsPage.js
+++ snowdrop-client/assets/PlantsPage/PlantsPage.js
@@ -1,23 +1,48 @@
 import React, { useState, useRef, useEffect } from "react";
-import { View, Text, ScrollView} from "react-native";
-import { Appbar } from 'react-native-paper';
+import { View, Text, ScrollView, Image, Dimensions } from "react-native";
+import { Appbar, Avatar, Card, FAB, IconButton } from 'react-native-paper';
 
 import styles from './PlantsPageStyle.js';
 
 const PlantsPage  = ({navigation}) => {
+    var width = Dimensions.get('window').width; 
+    var height = Dimensions.get('window').height;
 
 	return (
-    <View>
+    <View style={styles.container}>
     <Appbar.Header style={styles.appbar}>
-        <Appbar.BackAction/>
-        <Appbar.Action icon="settings"/>
+        <Appbar.BackAction color="white"/>
+        <Appbar.Action icon="brightness-5" color="white" style={{marginLeft: 'auto'}}/>
     </Appbar.Header>
-        
-	<ScrollView bounces={false} showsVerticalScrollIndicator={false}>
-		<View>
-            
+	<ScrollView style={styles.scroll} bounces={false} showsVerticalScrollIndicator={false}>
+        <View style={styles.ovalBg}>
+            <View style={styles.plantsView}>
+                <Text style={styles.plantsText}>Plants</Text>
+                <Image style={styles.plantsImage} source={require('snowdrop-client/assets/golden-pothos.png')}></Image>
+            </View>
+        </View>
+		<View style={styles.cardList}>
+            <Card.Title
+                style={styles.card}
+                title="Card Title"
+                subtitle="Card Subtitle"
+                left={(props) => <Avatar.Image {...props} size={width * 0.18} style={styles.cardImage} source={require('snowdrop-client/assets/golden-pothos.png')} />}
+                right={(props) => <IconButton {...props} icon="chevron-right" size={50} color={'#4E4E4E'} onPress={() => {}} />}
+            />
         </View>
 	</ScrollView>
+    <FAB
+        style={styles.fab}
+        icon="plus"
+        color="white"
+        onPress={() => console.log('Pressed')}
+    />
+    <Appbar style={styles.bottom}>
+        <Appbar.Action icon="home" color="#005500" size={width*0.09}/>
+        <Appbar.Action icon="leaf" color="#EDEECB" size={width*0.09} style={{marginLeft: '9%'}}/>
+        <Appbar.Action icon="account-supervisor" color="#005500" size={width*0.09} style={{marginLeft: '9%'}}/>
+        <Appbar.Action icon="brightness-5" color="#005500" size={width*0.09} style={{marginLeft: '9%'}}/>
+    </Appbar>
     </View>
 )}
 export default PlantsPage
--- snowdrop-client/assets/PlantsPage/PlantsPageStyle.js
+++ snowdrop-client/assets/PlantsPage/PlantsPageStyle.js
@@ -1,23 +1,92 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
 var width = Dimensions.get('window').width; 
-var height = Dimensions.get('window').height; 
+var height = Dimensions.get('window').height;
 
-var backgroundcolor = '#F2EBED';
+var defaultW = 414;
+var defaultH = 896;
+
+var backgroundcolor = '#EDEECB';
 var textcolor = '#2e2b36';
 var green = '#82B47D';
-var lightcolor = '#3F3F37';
+var blue = '#A8C1DD';
 
 export default StyleSheet.create({
     container: {
         width: width,
         height: height,
-        alignItems: 'center',
+        backgroundColor: backgroundcolor,
+    },
+    scroll: {
         backgroundColor: backgroundcolor,
     },
     appbar: {
         width: width,
         backgroundColor: green,
-    }
-
+        flexDirection: "row",
+        height: height * 0.078125, // 70/defaultH
+    },
+    ovalBg:{
+        alignSelf: 'center',
+        width: width / 4,
+        height: height * 0.234374, // 210/defaultH
+        backgroundColor: blue,
+        borderBottomLeftRadius: 90,
+        borderBottomRightRadius: 90,
+        transform: [{ scaleX: 4 }],
+    },
+    plantsView: {
+        flex: 1,
+        flexDirection: 'row',
+        //justifyContent: 'center',
+        alignItems: 'center',
+        transform: [{ scaleX: 1/4 }],
+    },
+    plantsText: {
+        justifyContent: 'flex-start',
+        color: 'white',
+        fontSize: 36,
+        letterSpacing: -0.24,
+        fontWeight: '500',
+    },
+    plantsImage: {
+        width: width * 0.6,
+        height: width * 0.6,
+    },
+    cardList: {
+        marginVertical: height * 0.05,
+    },
+    card: {
+        alignSelf: 'center',
+        backgroundColor: 'white',
+        width: width * 0.91787,
+        height: height * 0.1060,
+        borderRadius: 25,
+    },
+    cardImage: {
+        width: width * 0.18,
+        height: width * 0.18,
+        backgroundColor: 'white',
+        borderColor: 'grey',
+        borderWidth: 1,
+    },
+    fab: {
+        position: 'absolute',
+        //width: width * 0.1449,
+        //height: width * 0.1449,
+        marginBottom: height * 0.1,
+        marginRight: height * 0.02,
+        right: 0,
+        bottom: 0,
+        backgroundColor: green,
+    },
+    bottom: {
+        justifyContent: 'center',
+        backgroundColor: green,
+        height: height * 0.078125,
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
 });
