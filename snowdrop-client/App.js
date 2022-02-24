2980240744484dd1ccabfe39ce74e0878ea8a172
Ramya Mandyam Anandampullai <42981906+rmandyam1@users.noreply.github.com>
2022-02-24 13:12:45 -0500

    Additional plant details frontend

--- snowdrop-client/App.js
+++ snowdrop-client/App.js
@@ -1,15 +1,20 @@
+import { registerRootComponent } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Plant_Search from './pages/plants/Plant_Search'
+import Save_Plant from './pages/plants/Save_Plant'
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Plant_Search" component={Plant_Search} />
+        {/* <Stack.Screen name="Plant_Search" component={Plant_Search} /> */}
+        <Stack.Screen name="Save_Plant" component={Save_Plant} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+registerRootComponent(App);
